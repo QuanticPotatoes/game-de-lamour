72e0b50d385ab5ed35279cf692e8b0f721e0b69d
QuanticPotatoes <jeremy.dieuze@viacesi.fr>
2017-07-01 14:39:59 +0200

    :tada:

---
+++ cmake-build-debug/CMakeFiles/spaceGame.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/spaceGame.dir/main.cpp.o"
+  "spaceGame.pdb"
+  "spaceGame"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/spaceGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
